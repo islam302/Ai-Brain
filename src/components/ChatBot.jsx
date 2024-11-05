d42589167dd51b007c8412a6540e2ae1e6354bb5
islam302 <islambadran39@gmail.com>
2024-11-05 16:50:45 +0200

    add una news buttom

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -4,23 +4,21 @@ import { FiSend } from "react-icons/fi";
 import { GiReturnArrow } from "react-icons/gi";
 import HTMLParser from 'html-react-parser';
 import { TypeAnimation } from "react-type-animation";
-import AnimatedBackground from "./AnimatedBg";
 import "./ChatBot.css";
 
 const ChatPage = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
+  const [useUnaApi, setUseUnaApi] = useState(false); // حالة التبديل بين APIs
   const messagesEndRef = useRef(null);
 
-  // Scroll to bottom when messages change
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
   useEffect(() => {
-        document.title = "UNA BOOT";
-    }, []);
-
+    document.title = "UNA BOOT";
+  }, []);
 
   const addLinkTargetAttribute = (html) => {
     return html.replace(/<a /g, '<a target="_blank" rel="noopener noreferrer" ');
@@ -34,45 +32,71 @@ const ChatPage = () => {
     setMessages(newMessages);
     setInput("");
 
+    const apiUrl = useUnaApi ? "http://127.0.0.1:8000/api/ask_openai/" : "https://unachatbot.onrender.com/ask_questions/";
+
     try {
-      const response = await axios.post("https://unachatbot.onrender.com/ask_questions/", { question: input });
+      const response = await axios.post(apiUrl, { question: input });
       const updatedMessages = [...newMessages];
 
-      // Handle similar questions
-      if (
-        response.data.similar_questions &&
-        response.data.similar_questions.length > 0
-      ) {
-        updatedMessages.push({
-          text: ":هل تقصد",
-          sender: "bot",
-          icon: "https://i.postimg.cc/YSzf3QQx/chatbot-1.png",
-        });
-
-        // إضافة الأزرار فقط للأسئلة المشابهة بدون إضافة نص الرسالة
-        response.data.similar_questions.forEach((q) => {
-          updatedMessages.push({
-            sender: "bot",
-            id: q.id,
-            text: q.question, // هذا هو النص الذي يظهر على الزر فقط
-            isButton: true,
-            isHtml: false,
+      if (useUnaApi) {
+        // التعامل مع واجهة API ask_openai
+        if (response.data.answer && response.data.answer.length > 0) {
+          response.data.answer.forEach((answer) => {
+            updatedMessages.push({
+              text: `
+                <div>
+                  <h3>${answer.title}</h3>
+                  <p>${answer.content}</p>
+                  <a href="${answer.link}" target="_blank" rel="noopener noreferrer">اقرأ المزيد</a>
+                  <br>
+                  <img src="${answer.image_url}" alt="Image" style="width: 100%; height: auto; margin-top: 10px;">
+                </div>
+              `,
+              sender: "bot",
+              icon: "https://i.postimg.cc/YSzf3QQx/chatbot-1.png",
+              isHtml: true,
+            });
           });
-        });
-
-      } else if (response.data.answer) {
-        updatedMessages.push({
-          text: addLinkTargetAttribute(response.data.answer),
-          sender: "bot",
-          icon: "https://i.postimg.cc/YSzf3QQx/chatbot-1.png",
-          isHtml: true,
-        });
+        } else {
+          updatedMessages.push({
+            text: "آسف، لم أتمكن من العثور على إجابة.",
+            sender: "bot",
+            icon: "https://i.postimg.cc/wB80F6Z9/chatbot.png",
+          });
+        }
       } else {
-        updatedMessages.push({
-          text: "آسف، لم أتمكن من العثور على الإجابة.",
-          sender: "bot",
-          icon: "https://i.postimg.cc/wB80F6Z9/chatbot.png",
-        });
+        // التعامل مع واجهة API ask
+        if (response.data.similar_questions && response.data.similar_questions.length > 0) {
+          updatedMessages.push({
+            text: ":هل تقصد",
+            sender: "bot",
+            icon: "https://i.postimg.cc/YSzf3QQx/chatbot-1.png",
+          });
+
+          // إضافة أزرار للأسئلة المشابهة
+          response.data.similar_questions.forEach((q) => {
+            updatedMessages.push({
+              sender: "bot",
+              id: q.id,
+              text: q.question,
+              isButton: true,
+              isHtml: false,
+            });
+          });
+        } else if (response.data.answer) {
+          updatedMessages.push({
+            text: addLinkTargetAttribute(response.data.answer),
+            sender: "bot",
+            icon: "https://i.postimg.cc/YSzf3QQx/chatbot-1.png",
+            isHtml: true,
+          });
+        } else {
+          updatedMessages.push({
+            text: "آسف، لم أتمكن من العثور على الإجابة.",
+            sender: "bot",
+            icon: "https://i.postimg.cc/wB80F6Z9/chatbot.png",
+          });
+        }
       }
 
       setMessages(updatedMessages);
@@ -90,7 +114,6 @@ const ChatPage = () => {
   };
 
   const handleSimilarQuestion = async (id) => {
-    // ابحث عن السؤال المشابه باستخدام ID
     const similarQuestion = messages.find((msg) => msg.id === id);
     if (!similarQuestion) return;
 
@@ -99,19 +122,17 @@ const ChatPage = () => {
         question: similarQuestion.text,
       });
 
-      // إضافة الرسالة الجديدة كمحادثة من المستخدم
       const newMessages = [
         ...messages,
         { text: similarQuestion.text, sender: "user" },
       ];
 
-      // تحقق من وجود إجابة وتحديث الرسائل
       if (response.data && response.data.answer) {
         newMessages.push({
           text: response.data.answer,
           sender: "bot",
           icon: "https://i.postimg.cc/YSzf3QQx/chatbot-1.png",
-          isHtml: true, // تعيين النوع كـ HTML لعرض الروابط
+          isHtml: true,
         });
       } else {
         newMessages.push({
@@ -136,8 +157,7 @@ const ChatPage = () => {
   };
 
   const startListening = () => {
-    const SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
     if (!SpeechRecognition) {
       alert("Speech recognition is not supported in this browser.");
@@ -163,8 +183,11 @@ const ChatPage = () => {
     recognition.start();
   };
 
+  const handleNewsButtonClick = () => {
+    setUseUnaApi(!useUnaApi);
+  };
+
   return (
-    <AnimatedBackground>
       <div className="chat-page">
         <form onSubmit={sendMessage} className="chat-input-form">
           <input
@@ -173,7 +196,10 @@ const ChatPage = () => {
               onChange={(e) => setInput(e.target.value)}
               placeholder="...اكتب سؤالك هنا"
               className="chat-input"
-              style={{textAlign: "right"}}
+              style={{
+                textAlign: "right",
+                backgroundColor: useUnaApi ? "#e0f7da" : "", // لون مختلف عند تفعيل أخبار UNA
+              }}
           />
           <button type="submit" className="send-button">
             <FiSend/>
@@ -192,6 +218,18 @@ const ChatPage = () => {
                 }}
             />
           </button>
+          <button
+              type="button"
+              onClick={handleNewsButtonClick}
+              className={`news-button una-news-button ${useUnaApi ? 'pressed' : ''}`}
+              style={{
+                backgroundColor: useUnaApi ? "#023f31" : "#07755DFF", // لون مختلف حسب الحالة
+                color: useUnaApi ? "#ffffff" : "#ffffff", // لون نص مختلف
+              }}
+          >
+            اخبار من يونا
+          </button>
+
         </form>
         <div className="chat-header">
           <h1>UNA BOOT</h1>
@@ -201,36 +239,33 @@ const ChatPage = () => {
           <div className="chat-messages">
             {messages.map((msg, index) => (
                 <div key={index} className={`chat-message ${msg.sender}`}>
-                  <div className="message-text">
-                    {msg.isHtml ? (
-                        <div>{HTMLParser(msg.text)}</div>
-                    ) : msg.isButton ? null : ( // لا يتم عرض النص كرسالة إذا كان msg.isButton صحيحاً
-                        <TypeAnimation
-                            sequence={[msg.text, () => {}]}
-                            speed={70}
-                            repeat={0}
-                            wrapper="div"
-                        />
-                    )}
-                  </div>
-                  {msg.sender === "bot" && msg.isButton && (
-                      <button
-                          onClick={() => handleSimilarQuestion(msg.id)}
-                          className="similar-question-button"
-                      >
-                        <GiReturnArrow/> {msg.text}
-                      </button>
+                <div className="message-text">
+                  {msg.isHtml ? (
+                    <div>{HTMLParser(msg.text)}</div>
+                  ) : msg.isButton ? null : (
+                    <TypeAnimation
+                      sequence={[msg.text, () => {}]}
+                      speed={70}
+                      repeat={0}
+                      wrapper="div"
+                    />
                   )}
                 </div>
+                {msg.sender === "bot" && msg.isButton && (
+                  <button
+                    onClick={() => handleSimilarQuestion(msg.id)}
+                    className="similar-question-button"
+                  >
+                    <GiReturnArrow /> {msg.text}
+                  </button>
+                )}
+              </div>
             ))}
-
-            <div ref={messagesEndRef}/>
+            <div ref={messagesEndRef} />
           </div>
         </div>
-
-        <img src="../rob.png" alt="" className="robot-container"/>
+        <img src="../rob.png" alt="" className="robot-container" />
       </div>
-    </AnimatedBackground>
   );
 };
 
