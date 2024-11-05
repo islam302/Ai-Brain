13a3909b355c1aef0c723bec63b3111fceed955a
islam302 <islambadran39@gmail.com>
2024-11-06 01:07:45 +0200

    update ask-una-api

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -32,7 +32,7 @@ const ChatPage = () => {
     setMessages(newMessages);
     setInput("");
 
-    const apiUrl = useUnaApi ? "http://127.0.0.1:8000/api/ask_openai/" : "https://unachatbot.onrender.com/ask_questions/";
+    const apiUrl = useUnaApi ? "https://unachatbot.onrender.com/ask_una/" : "https://unachatbot.onrender.com/ask_questions/";
 
     try {
       const response = await axios.post(apiUrl, { question: input });
